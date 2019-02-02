af5f6aae13c7ca836d3722daf871cccc7dcf7d00
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-02-01 22:45:58 -0500

    added unix timestamp to response

--- app/controllers/api/timestamp_controller.rb
+++ app/controllers/api/timestamp_controller.rb
@@ -1,11 +1,11 @@
 module Api
   class TimestampController < ApiController
-    def index
-      render json: { "utc" => Time.new.httpdate }
-    end
+    # def index
+    #   render json: { "unix" => Time.new.millisecond, "utc" => Time.new }
+    # end
     def show
       if !params[:string]
-        render json: { "utc" => Time.new.httpdate }
+        render json: { "unix" => DateTime.now.strftime("%Q"), "utc" => DateTime.now.httpdate }
       else
         input_time = params[:string].split('-').map { |num| num.to_i }
         render json: { "utc" => Time.gm(*input_time).httpdate }
