1fe9eb73b976aafa89a58ed1c91a75f6c25f47ab
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-02-01 23:12:47 -0500

    added support for unix input and added error message

--- app/controllers/api/timestamp_controller.rb
+++ app/controllers/api/timestamp_controller.rb
@@ -3,12 +3,20 @@ module Api
     def index
       render json: { "unix" => DateTime.now.strftime("%Q"), "utc" => DateTime.now.httpdate }
     end
+
     def show
       if !params[:string]
         render json: { "unix" => DateTime.now.strftime("%Q"), "utc" => DateTime.now.httpdate }
       else
         input_time = params[:string].split('-').map { |num| num.to_i }
-        render json: { "unix" => DateTime.new(*input_time).strftime("%Q"), "utc" => DateTime.new(*input_time).httpdate }
+
+        if input_time.length == 1
+          render json: { "unix" => input_time[0], "utc" => DateTime.strptime(input_time[0].to_s, "%Q") }
+        elsif input_time.length == 3
+          render json: { "unix" => DateTime.new(*input_time).strftime("%Q"), "utc" => DateTime.new(*input_time).httpdate }
+        else
+          render json: { "error" => "Invalid Date" }
+        end
       end
     end
   end
