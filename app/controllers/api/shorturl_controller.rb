cdbeb7486418d765c63c2278ca46263f0a3b8c19
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-02-03 22:26:53 -0500

    added dns lookup to verify url before creating a shorturl

--- app/controllers/api/shorturl_controller.rb
+++ app/controllers/api/shorturl_controller.rb
@@ -1,9 +1,18 @@
+require 'resolv.rb'
 class Api::ShorturlController < ApiController
   def show
     @shorturl = Shorturl.find(params[:id])
-    render json: @shorturl
+    redirect_to @shorturl[:original_url]
   end
+
   def create
+    begin
+      address = Resolv.getaddress(params[:url])
+      res = Resolv.getname(address)
+      rescue Resolv::ResolvError => e
+        render json: {"error" => "invalid URL"}
+        return
+    end
     @shorturl = Shorturl.new("original_url" => params[:url])
     if @shorturl.save
       render json: { "original_url" => @shorturl[:original_url], "short_url" => @shorturl[:id] }
