046088b6ca427022849295cbd9e5b5e78d99d530
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-02-04 18:44:27 -0500

    added strong params for exercise controller

--- app/controllers/api/exercisetracker/exercises_controller.rb
+++ app/controllers/api/exercisetracker/exercises_controller.rb
@@ -3,5 +3,12 @@ class Api::Exercisetracker::ExercisesController < ApiController
     render json: Exercise.where(user_id: params[:user_id])
   end
   def create
+    render json: exercise_params
+  end
+
+  private
+
+  def exercise_params
+    params.permit(:description, :duration, :user_id, :date)
   end
 end
