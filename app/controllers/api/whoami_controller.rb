f23ef935473868cd795f6919fa5fc56273d9e5c5
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-02-03 20:21:18 -0500

    completed whoami api

--- app/controllers/api/whoami_controller.rb
+++ app/controllers/api/whoami_controller.rb
@@ -1,5 +1,9 @@
 class Api::WhoamiController < ApiController
   def index
-
+    render json: {
+      "ipaddress" => request.ip,
+      "language" => request.headers["Accept-Language"],
+      "software" => request.headers["User-Agent"]
+    }
   end
 end
