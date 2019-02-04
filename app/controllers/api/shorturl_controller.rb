49c09ad16e1e9066ed01e48b8e08e1ca377e9a2f
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-02-03 21:40:41 -0500

    added error message to create action

--- app/controllers/api/shorturl_controller.rb
+++ app/controllers/api/shorturl_controller.rb
@@ -1,13 +1,15 @@
 class Api::ShorturlController < ApiController
-  def index
-    render json: Shorturl.all
+  def show
+    render params
+    # Shorturl.find(params[:id])
+    # render json: Shorturl.all
   end
   def create
     @shorturl = Shorturl.new("original_url" => params[:url])
     if @shorturl.save
       render json: { "original_url" => @shorturl[:original_url], "short_url" => @shorturl[:id] }
     else
-      render json: "error occurred"
+      render json: @shorturl.errors.full_messages, status: :unprocessable_entity
     end
   end
 end
