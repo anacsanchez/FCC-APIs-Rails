05de6b5fda088478f78938466d1f504bb42c24d0
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-02-04 21:20:46 -0500

    default params and different to and from dates work

--- app/controllers/api/exercisetracker/exercises_controller.rb
+++ app/controllers/api/exercisetracker/exercises_controller.rb
@@ -1,11 +1,16 @@
 class Api::Exercisetracker::ExercisesController < ApiController
   def index
-
-    # if query_params
-    #   render json: Exercise.where(date: query_params[:to]
-    # end
-    # render json: Exercise.where(user_id: params[:user_id])
+    if !query_params.empty?
+      from =  query_params.fetch(:from) { Date.new.to_s }
+      to = query_params.fetch(:to) {Date.today.to_s}
+      limit = query_params.fetch(:limit) { nil }
+      render json: Exercise.where(date: (from..to)).limit(limit)
+      # render json: query_params
+    else
+      render json: Exercise.where(user_id: params[:user_id])
+    end
   end
+
   def create
     @exercise = Exercise.new(exercise_params)
     if @exercise.save
